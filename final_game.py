93e37ed4d14acf7f3abf7e8a08920995d7118d05
Suman Khadka <sk394@uakron.edu>
2023-02-20 17:42:35 -0500

    Updated the markdown file

--- final_game.py
+++ final_game.py
@@ -2,10 +2,23 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import simpledialog
 from tkinter import Menu
+import os
+import sys
 import random  #for generating random player symbol
 
+#https://stackoverflow.com/questions/31836104/pyinstaller-and-onefile-how-to-include-an-image-in-the-exe-file
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS2
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 root=tk.Tk() #creating root window
-root.iconbitmap('tictac.ico') #setting icon for root window
+root.iconbitmap(resource_path('data\\tictac.ico')) #setting icon for root window
 root.resizable(False,False)
 root.title('Tic Tac Toe Game') #setting title for root window
 
